3754335a2e0a72af895bae7ee635bc2c7ea03155
hotanthanhnhan <hotanthanhnhan@gmail.com>
2021-07-14 03:42:13 +0700

    LoginBank

---
+++ LAB211/LoginBank/src/loginbank/Controller.java
@@ -0,0 +1,109 @@
+/*
+ * Hồ Tấn Thành Nhân
+ * DE150039
+ * SE1505
+ */
+
+package loginbank;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Controller extends Menu{
+
+    Login L = new Login();
+    Scanner sc = new Scanner(System.in);
+    Locale localeVi = new Locale("vi");
+    ResourceBundle vi = ResourceBundle.getBundle("language.language", localeVi);
+    Locale localeEn = new Locale("en");
+    ResourceBundle en = ResourceBundle.getBundle("language.language", localeEn);
+    
+    @Override
+    void excute(int n) {
+        switch (n) {
+            case 1:{
+                this.loginVi();
+                break;
+            }
+            case 2:{
+                this.loginEn();
+                break;
+            }     
+            case 3:{
+                System.exit(0);
+            }
+            default: System.err.println("No invalid.");              
+        }
+    }
+
+     void loginVi(){
+        System.out.print(vi.getString("Account"));
+        L.setAccount(sc.nextLine().trim());
+        while (!Validation.checkAccount(L.getAccount())) {
+            System.err.println(vi.getString("AccountError"));
+            System.out.print(vi.getString("Account"));
+            L.setAccount(sc.nextLine().trim());
+        }
+        System.out.print(vi.getString("Pass"));
+        L.setPass(sc.nextLine().trim());
+        while (!Validation.checkPass(L.getPass())) {
+            System.err.println(vi.getString("PassError"));
+            System.out.print(vi.getString("Pass"));
+            L.setPass(sc.nextLine().trim());
+        }
+        L.setCaptcha();
+        System.out.println("Captcha: "+L.getCaptcha());
+        System.out.print(vi.getString("Captcha"));
+        String resCaptcha = sc.nextLine().trim();
+        while (!resCaptcha.equals(L.getCaptcha())) {
+            System.err.println(vi.getString("CaptchaError"));
+            System.out.print(vi.getString("Captcha"));
+            resCaptcha = sc.nextLine().trim();
+        }
+    }
+
+    private void loginEn(){
+        System.out.print(en.getString("Account"));
+        L.setAccount(sc.nextLine().trim());
+        while (!Validation.checkAccount(L.getAccount())) {
+            System.err.println(en.getString("AccountError"));
+            System.out.print(en.getString("Account"));
+            L.setAccount(sc.nextLine().trim());
+        }
+        System.out.print(en.getString("Pass"));
+        L.setPass(sc.nextLine().trim());
+        while (!Validation.checkPass(L.getPass())) {
+            System.err.println(en.getString("PassError"));
+            System.out.print(en.getString("Pass"));
+            L.setPass(sc.nextLine().trim());
+        }
+        L.setCaptcha();
+        System.out.println("Captcha: "+L.getCaptcha());
+        System.out.print(en.getString("Captcha"));
+        String resCaptcha = sc.nextLine().trim();
+        while (!resCaptcha.equals(L.getCaptcha())) {
+            System.err.println(en.getString("CaptchaError"));
+            System.out.print(en.getString("Captcha"));
+            resCaptcha = sc.nextLine().trim();
+        }
+    }
+
+    @Override
+    int select() {
+        this.displayMenu();
+        return Validation.CheckMinMax(1, 3);
+    }
+
+    void Run(){
+        while (true) {
+            int n = this.select();
+            this.excute(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Controller ctrl = new Controller();
+        ctrl.Run();
+    }
+}
---
+++ LAB211/LoginBank/src/loginbank/Menu.java
@@ -0,0 +1,48 @@
+/*
+ * Hồ Tấn Thành Nhân
+ * DE150039
+ * SE1505
+ */
+
+package loginbank;
+
+import java.util.ArrayList;
+
+public abstract class Menu {
+    private String Title;
+    private final ArrayList Menu = new ArrayList<>();
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public final void setTitle() {
+        this.Title = "-------Login Program-------";
+    }
+
+    public ArrayList getMenu() {
+        return Menu;
+    }
+
+    public final void setMenu() {
+        this.Menu.add("Vietnamese");
+        this.Menu.add("English");        
+        this.Menu.add("Exit");
+    }
+
+    public Menu() {
+        this.setTitle();
+        this.setMenu();
+    }
+
+    public void displayMenu(){
+        System.out.println(this.getTitle());
+        for (int i = 0; i < Menu.size(); i++) {
+            System.out.println((i+1)+". "+Menu.get(i));
+        }
+    }
+
+    abstract void excute(int n);
+
+    abstract int select();
+}
